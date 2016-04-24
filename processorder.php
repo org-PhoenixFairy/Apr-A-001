de33f5a7a505e9c9bd70366b3f3bcae3e25ff6a3
PhoenixFairy <microsoft@inno-sp.com>
2016-04-24 10:11:04 +0800

    the end

--- processorder.php
+++ processorder.php
@@ -3,6 +3,12 @@
 $tireqty = $_POST[ 'tireqty'];
 $oilqty = $_POST[ 'oilqty'];
 $sparkqty = $_POST[ 'sparkqty'];
+$address = $_POST[ 'address'];
+$DOCUMENT_ROOT = $_SERVER[ 'DOCUMENT_ROOT'];
+$date = date('H:i ,jS F Y');
+define('TIREPRICE', 100);#定义常量--价格
+define('OILPRICE', 10);
+define('SPARKPRICE', 4);
 ?>
 <html>
 <head>
@@ -15,26 +21,51 @@ $sparkqty = $_POST[ 'sparkqty'];
     #计算表单总金额
     $totalqty = 0;
     $totalqty = $tireqty + $oilqty + $sparkqty;//计算表单总量
-    define('TIREPRICE', 100);#定义常量--价格
-    define('OILPRICE', 10);
-    define('SPARKPRICE', 4);
     
-    echo "<p>Order processed at ";
-    echo date('H:i ,jS F Y');
-    echo "</p>";//保存日期
+    echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
+    echo "<p>Your order is as follows :</p>";
     
     echo "Items ordered: ".$totalqty."<br />";
+    if($totalqty == 0){
+        echo "You did not order anything on the previous page!<br />";
+    } else {
+        if($tireqty > 0){
+            echo $tireqty." tires<br />";
+        }
+        if ($oilqty > 0){
+            echo $oilqty." bottles of oil<br />";
+        }
+        if($sparkqty > 0){
+            echo $sparkqty." sparl plugs<br />";
+        }
+    }
     $totalamount = 0.00;//
     
     $totalamount = $tireqty * TIREPRICE
                  + $oilqty * OILPRICE
                  + $sparkqty * SPARKPRICE;//计算表单总金额
+    $totalamount=number_format($totalamount,2,'.',' ');
     
+    echo "<p>Total of order is $".$totalamount."</p>";
+    echo "<p>Address to ship to is ".$address."</p>";
     
-    $taxrate = 0.10;// local sales tax is 10%     基本税价 = 10%。        
-    echo "Subtotal: $".number_format($totalamount,2)."<br />";
-    $totalamount = $totalamount * (1 + $taxrate);
-    echo "Total including tax : $".number_format($totalamount,2)."<br />";   
+    $outputstring = $date."\t".$tireqty." tires \t".$oilqty." oil\t"
+                    .$sparkqty." spark plugs\t\$".$totalamount
+                    ."\t".$address."\n";
+    //open file for appending
+    @ $fp = fopen("$DOCUMENT_ROOT/../orders/orders.txt", 'ab');
+    flock($fp, LOCK_EX);
+
+    if(!fp) {
+        echo "<p><strong> Your order could not be processed at this time.
+            Please try again later.</strong></p></body></html>";
+        exit;
+    }
+    fwrite($fp, $outputstring,strlen($outputstring));
+    flock($fp, LOCK_UN);
+    fclose($fp);
+    
+    echo "<p>Order written.</p>";
 ?>
 </body>
 </html>
